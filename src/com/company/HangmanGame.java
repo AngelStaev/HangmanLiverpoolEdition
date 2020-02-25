7a8442de7a981b2a220c7b40ebb7600864bf93ed
Angel Staev <angel_staev@abv.bg>
2020-02-25 23:01:05 +0200

    Methods for new game and words compare

--- src/com/company/HangmanGame.java
+++ src/com/company/HangmanGame.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class HangmanGame {
@@ -18,6 +19,25 @@ public class HangmanGame {
     String wordToFind;
     char[] wordFound;
 
+    ArrayList<String> letters = new ArrayList<>();
+
+    public void newGame() {
+
+        errors = 0;
+        letters.clear();
+        wordToFind = words[random.nextInt()];
+        wordFound = new char[wordToFind.length()];
+
+        for (int i = 0; i < wordToFind.length(); i++) {
+            wordFound[i] = '_';
+        }
+    }
+
+    public boolean isWordFound() {
+
+        return wordToFind.contentEquals(new String(wordFound));
+        
+    }
 
     public static void main(String[] args) {
 
